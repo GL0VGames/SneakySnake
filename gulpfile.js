dadef32794b3a2f1e3daccdecbf60bc444971c50
Graham Barber <grahammbarber@gmail.com>
2015-06-06 14:36:54 -0700

    Created and tested gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var ts = require("gulp-typescript");
+var replace = require("gulp-html-replace");
+var minHTML = require("gulp-minify-html");
+var minCSS = require("gulp-minify-css")
+var concat = require("gulp-concat");
+var imageMin = require("gulp-imagemin");
+
+gulp.task("default", function () {
+//	Select the HTML index, replace the script sources, minify, and move to dist	
+	gulp.src("SS/SS/index.html")
+		.pipe(replace({
+			jquery: "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js \"></script>",
+			js: "<script src=\"js/index.js\"></script>"
+		}))
+		.pipe(minHTML())
+		.pipe(gulp.dest("dist/"));
+		
+//	Select the CSS, minify, and move to dist		
+	gulp.src("SS/SS/css/*.css")
+		.pipe(minCSS())
+		.pipe(gulp.dest("dist/css/"));
+		
+	gulp.src(["SS/SS/js/ArgyleEngine.ts", "SS/SS/js/game.ts", "SS/SS/js/NPC.js", "SS/SS/js/player.ts", "SS/SS/js/Teleporter.ts"])
+		.pipe(concat("index.ts"))
+		.pipe(ts())
+		.pipe(uglify())
+		.pipe(gulp.dest("dist/js/"));
+		
+	gulp.src("SS/SS/images/*")
+		.pipe(imageMin())
+		.pipe(gulp.dest("dist/images/"));
+		
+	gulp.src("SS/SS/sounds/*")
+		.pipe(gulp.dest("dist/sounds/"));
+	
+});
