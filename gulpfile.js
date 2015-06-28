4b6688b3be1370be77487ad894acf5d6d7ab73d5
Graham Barber <grahammbarber@gmail.com>
2015-06-28 09:55:03 -0700

    Changed gulp deploy for new serv

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var rsync = require("gulp-rsync");
 //var obfuscate = require("gulp-obfuscate");
 
 gulp.task("build", function () {
-//	Select the HTML index, replace the script sources, minify, and move to dist	
+//	Select the HTML index, replace the script sources, minify, and move to dist
 	gulp.src("SS/SS/index.html")
 		.pipe(replace({
 			jquery: "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js \"></script>",
@@ -18,34 +18,34 @@ gulp.task("build", function () {
 		}))
 		.pipe(minHTML())
 		.pipe(gulp.dest("dist/"));
-		
-//	Select the CSS, minify, and move to dist		
+
+//	Select the CSS, minify, and move to dist
 	gulp.src("SS/SS/css/*.css")
 		.pipe(minCSS())
 		.pipe(gulp.dest("dist/css/"));
-		
+
 	gulp.src(["SS/SS/js/ArgyleEngine.ts", "SS/SS/js/game.ts", "SS/SS/js/NPC.js", "SS/SS/js/player.ts", "SS/SS/js/Teleporter.ts"])
 		.pipe(concat("index.ts"))
 		.pipe(ts())
 //		.pipe(obfuscate())
 		.pipe(uglify())
 		.pipe(gulp.dest("dist/js/"));
-		
+
 	gulp.src("SS/SS/images/*")
 		.pipe(imageMin())
 		.pipe(gulp.dest("dist/images/"));
-		
+
 	gulp.src("SS/SS/sounds/*")
 		.pipe(gulp.dest("dist/sounds/"));
-	
+
 });
 
-gulp.task("deploy", function () {
+gulp.task("deploy", ["build"], function () {
 	gulp.src("dist/")
 		.pipe(rsync({
 			root: "dist",
 			hostname: "gl0vgames.com",
-			destination: "/var/web/ss/",
+			destination: "/usr/share/nginx/ss/",
 			username: "root",
 			incremental: true,
 			progress: true,
